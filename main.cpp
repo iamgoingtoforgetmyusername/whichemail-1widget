15d638703fff7cd91ba9377aa4a525f951b3da5d
iamgoingtoforgetmyusername <54243436+iamgoingtoforgetmyusername@users.noreply.github.com>
2021-01-18 19:52:44 +0100

    remove old comments in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include "mainwindow.h"
-//#include "retrieveEmail.h"
 
 #include <QApplication>
 #include <iostream>
@@ -11,10 +10,5 @@ int main(int argc, char *argv[])
     w.setGeometry(500,400,303,164);
     w.show();
 
- //   if (w.centralWidget()->widget->isHidden()){
- //   QString acctName = w.getAcctNo();
-   // std::string utf8_acctno = acctName.toUtf8().constData();
-//std::cout << "test" << utf8_acctno << std::endl;
- //   }
     return a.exec();
 }
